ed08af301e9afad9937f657243e92de61b24f0ab
Jeison Moncada <danielmoncada492@gmail.com>
2023-06-30 06:52:56 -0500

    tarea completada

--- variables_python.py
+++ variables_python.py
@@ -9,11 +9,14 @@ print("La suma de", n , "+" , c , "es igual a" , h , "pero si lo aproximamos es"
 Los tipos de datos enteros para Python 3 no presentan limite de tamaño
 Los datos flotantes tienen un limite de tamaño de 53 bits
 """
-f=0
+p=0
 n=0
 print ("ingresa un numero entero")
 n = int(input())
 while n >= 0:
-    if  
-    f= (i*(i+1)/2)
-print(f)
+    if  n % 2 == 0:
+        p = p + n
+        n -= 1
+    else:
+        n -= 1
+print("La suma de los pares del numero ingreso es ", p)
