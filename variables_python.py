670f112b034585b84ea447b05d262fccd6ee22cb
Jeison Moncada <danielmoncada492@gmail.com>
2023-06-30 06:32:00 -0500

    completo ejercicio

--- variables_python.py
+++ variables_python.py
@@ -1,2 +1,19 @@
-nombre = int(input())
-print("hola", nombre)
+a = 2
+b = 2.3
+n = str(a)
+c = str(b)
+h = (a + b )
+f= int(h)
+print("La suma de", n , "+" , c , "es igual a" , h , "pero si lo aproximamos es", f)
+"""
+Los tipos de datos enteros para Python 3 no presentan limite de tamaño
+Los datos flotantes tienen un limite de tamaño de 53 bits
+"""
+f=0
+n=0
+print ("ingresa un numero entero")
+n = int(input())
+while n >= 0:
+    if  
+    f= (i*(i+1)/2)
+print(f)
